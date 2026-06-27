941c5be8e2c948710d349499a6d603a06654ce9e
ashishdalvi2476-svg <ashishdalvi2476@gmail.com>
2026-06-27 17:24:26 +0530

    Add optimal triplet finding function in 3Sum.cpp
    Implemented an optimal approach to find triplets that sum to a given value in the 3Sum problem.

--- Array/Hard/3Sum.cpp
+++ Array/Hard/3Sum.cpp
@@ -49,3 +49,32 @@ vector<vector<int>> findTriplets(vector<int>arr, int n, int x) {
 
 // Optimal Approach
 
+#include <bits/stdc++.h> 
+vector<vector<int>> findTriplets(vector<int>arr, int n, int x) {
+		vector<vector<int>> ans;
+
+        sort(arr.begin(),arr.end());
+
+    for(int i=0;i<n;i++){
+        if(i>0 && arr[i]==arr[i-1]){ continue; }
+
+        int j=i+1,k=n-1;
+        while(j<k){
+            int sum=arr[i]+arr[j]+arr[k];
+
+            if(sum<x){ j++; }
+            else if(sum>x){ k--; }
+            else{
+                vector<int> temp={arr[i],arr[j],arr[k]};
+                ans.push_back(temp);
+                j++;
+                k--;
+
+                while(j<k && arr[j-1]==arr[j]){ j++; }
+                while(j<k && arr[k+1]==arr[k]){ k--; }
+            }
+        }
+    }
+
+    return ans;
+}
